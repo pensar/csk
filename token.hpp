e85d5675f245b243d7607b1110b12ce2ae16822f
Mauricio Gomes <mgpensar@gmail.com>
2023-03-23 08:38:21 -0300

    Added token classes.

--- token.hpp
+++ token.hpp
@@ -31,9 +31,54 @@ class Token : public Name
         unsigned int64_t column;
  };
 
+ /// Keywords are words that have special meaning to the CSK compiler.
+ /// These are reserved words and they cannot be used as identifiers.
  class Keyword: Token
  {
-     Keyword (const Id akeyword_id = NULL_ID, const std::string& skeyword = ""): Token (akeyword_id, skeyword) {}
+    public:
+        Keyword (const Id akeyword_id = NULL_ID, const std::string& skeyword = ""): Token (akeyword_id, skeyword) {}
+ };
+
+ /// Identifiers are the names for variables, types, functions, and labels in a CSK program.
+ class Identifier: Token
+ {
+    public:
+        Identifier (const Id aidentifier_id = NULL_ID, const std::string& sidentifier = ""): Token (aidentifier_id, sidentifier) {}
+ };
+
+ /// A string literal is a sequence of characters enclosed in double quotation marks (" ").
+ class StringLiteral: Token
+ {
+    public:
+        StringLiteral (const Id astring_literal_id = NULL_ID, const std::string& astr = ""): Token (astring_literal_id, astr) {}
+ };
+
+ /// A constant is a number, character, or character string that can be used as a value in a program and cannot be changed.
+ /// \see FloatingPointConstant
+ /// \see IntegerConstant
+ /// \see ListConstant
+ /// \see CharacterConstant
+ class Constant: Token
+ {
+    public:
+        Constant (const Id aconstant_id = NULL_ID, const std::string& sconstant = ""): Token (aconstant_id, sconstant) {}
+ };
+
+ /// Operators specify an evaluation to be performed on one or more operands.
+ class Operator: Token
+ {
+    public:
+        Operator (const Id aoperator_id = NULL_ID, const std::string& soperator = ""): Token (aoperator_id, aoperator_id) {}
+ };
+
+
+ /// Punctuators and special characters are used to
+ /// They are:  ( ) [ ] { } * , : ...
+ class Punctuator: Token
+ {
+    public:
+        Punctuator (const Id apunctuator_id = NULL_ID, const std::string& spunctuator = ""): Token (apunctuator_id, spunctuator) {}
  };
 
 #endif // TOKEN_HPP
+
